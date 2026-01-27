13690ad8f8c2a8ff1c77103dd52b67d010e365a9
Beerdavindersingh <beerdavindersingh@rrc.com>
2026-01-26 23:48:29 -0600

    lab 2.1 created

--- lab1.2/src/App.tsx
+++ lab1.2/src/App.tsx
@@ -1,6 +1,48 @@
+import { useState } from "react";
 import EmployeeList from "./components/employeeList";
+import EmployeeForm from "./components/employeeForm";
+
+interface Employee {
+  firstName: string;
+  lastName?: string;
+}
+
+interface Department {
+  name: string;
+  employees: Employee[];
+}
 
 function App() {
+  const [departments, setDepartments] = useState<Department[]>([
+    {
+      name: "Administration",
+      employees: [
+        { firstName: "Zoë", lastName: "Robins" },
+        { firstName: "Madeleine", lastName: "Madden" }
+      ]
+    },
+    {
+      name: "Audit",
+      employees: [
+        { firstName: "Josha", lastName: "Sadowski" },
+        { firstName: "Kate", lastName: "Fleetwood" }
+      ]
+    }
+  ]);
+
+  function addEmployee(firstName: string, lastName: string, departmentName: string) {
+    setDepartments((prev) =>
+      prev.map((dept) =>
+        dept.name === departmentName
+          ? {
+              ...dept,
+              employees: [...dept.employees, { firstName, lastName }]
+            }
+          : dept
+      )
+    );
+  }
+
   return (
     <>
       <header>
@@ -8,13 +50,15 @@ function App() {
         <p>Welcome to the Pixell River Financial staff directory</p>
       </header>
 
-      <EmployeeList />
+      <EmployeeList departments={departments} />
+
+      <EmployeeForm
+        departments={departments.map((d) => d.name)}
+        onAddEmployee={addEmployee}
+      />
 
       <footer>
-        <p>
-          Copyright Pixell River Financial ©{" "}
-          {new Date().getFullYear()}
-        </p>
+        <p>Copyright Pixell River Financial © {new Date().getFullYear()}</p>
       </footer>
     </>
   );
